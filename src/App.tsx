a63d06ede205559ce55bec748f704885759a2cd7
pooja-seshadri <101956501+pooja-seshadri@users.noreply.github.com>
2024-03-03 23:17:00 +1100

    feat: add initial UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import './App.css'
-import Dropdown from './Dropdown'
+import CloudIcon from '@mui/icons-material/Cloud'
+import { Autocomplete, AutocompleteItem } from "@nextui-org/react"
 
 function App() {
-  const options = ['Option 1', 'Option 2', 'Option 3'];
-
+  const cities = [
+    { label: 'Sydney', value: 'sydney' },
+    { label: 'Melbourne', value: 'melbourne' },
+    { label: 'Paris', value: 'paris' },
+  ]
   return (
     <>
-      <div>
-        <h1 className='text-green-400'>Weather App</h1>
-        <Dropdown options={options} />
+      <div className='flex justify-center'>
+        <div className='pt-2 pr-4'>
+          <CloudIcon viewBox='0 0 24 24' fontSize='large'/>
+        </div>
+        <h1 className='font-thin text-4xl tracking-wide'>weather app</h1>
+      </div>
+      <div className='mt-4'>
+        <Autocomplete
+          allowsCustomValue
+          label="search for a city"
+          variant="bordered"
+          className="max-w-ws font-light"
+          defaultItems={cities}
+        >
+          {(item) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
+        </Autocomplete>
       </div>
     </>
   )
---
+++ src/strings.ts
@@ -0,0 +1,3 @@
+// const TITLES = {
+//   weatherApp = "weather app",
+// }
